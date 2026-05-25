8e31b00e4b7c09534609fdaa3e3fa9e959a4b9b1
rachitbaranwal <rachitb3104@gmail.com>
2026-05-25 10:11:53 +0530

    Concatenation of Array

---
+++ src/Ques_1929.java
@@ -0,0 +1,17 @@
+//Concatenation of Array
+
+import java.util.Arrays;
+
+public class Ques_1929 {
+    public static void main(String [] args){
+        int [] nums = {1, 2, 1};
+        int n = nums.length;
+        int [] ans = new int [2*n];
+        for(int i = 0; i<n; i++){
+            ans[i] = nums[i];
+            ans [i+n]= nums[i];
+        }
+        System.out.println(Arrays.toString(ans));
+        // [1, 2, 1, 1, 2, 1]
+    }
+}
